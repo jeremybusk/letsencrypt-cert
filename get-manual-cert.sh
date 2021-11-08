b295fdf513a2bdda411cc8fdff06ec46f508409f
Jeremy Busk <jeremy.busk@extendhealth.com>
2021-11-08 10:41:23 -0700

    Update get-manual-cert.sh

--- get-manual-cert.sh
+++ get-manual-cert.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
-domain="example.com"
-certbot certonly --agree-tos --no-bootstrap --manual --manual-public-ip-logging-ok --manual-auth-hook ./authenticate --manual-cleanup-hook ./cleanup --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory -d "*.${domain}"
-sudo ls /etc/letsencrypt/live/${domain}
+set -eux
+# certbot certonly --agree-tos --no-bootstrap --manual --manual-public-ip-logging-ok --manual-auth-hook ./authenticate --manual-cleanup-hook ./cleanup --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory -d "*.${domain}"
+certbot certonly --agree-tos --no-bootstrap --manual --manual-public-ip-logging-ok --manual-auth-hook ./authenticate --manual-cleanup-hook ./cleanup --preferred-challenges dns -d "${CERT_CN}"
+echo "Get cert by domain in folder /etc/letsencrypt/live/"
