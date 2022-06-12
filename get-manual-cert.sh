bf8c0a309ca8f4baed4c3c26c7f766ebe3abfde7
Jeremy Busk <jeremy.busk@extendhealth.com>
2022-06-11 18:53:56 -0600

    refactor/allow arg for cn name

--- get-manual-cert.sh
+++ get-manual-cert.sh
@@ -1,5 +1,16 @@
-#!/usr/bin/env bash
-set -eux
-# certbot certonly --agree-tos --no-bootstrap --manual --manual-public-ip-logging-ok --manual-auth-hook ./authenticate --manual-cleanup-hook ./cleanup --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory -d "*.${domain}"
-certbot certonly --agree-tos --no-bootstrap --manual --manual-public-ip-logging-ok --manual-auth-hook ./authenticate --manual-cleanup-hook ./cleanup --preferred-challenges dns -d "${CERT_CN}"
-echo "Get cert by domain in folder /etc/letsencrypt/live/"
+#!/bin/bash
+set -eu
+
+if [[ "$#" != 1 ]]; then
+  echo "Usage: $0 <myhost.example.com>"
+  echo "Example: $0 myhost.dev.example.com"
+  exit
+fi
+export CERT_CN="$1"
+
+certbot certonly --agree-tos --no-bootstrap --manual \
+  --manual-public-ip-logging-ok --manual-auth-hook ./authenticate \
+  --manual-cleanup-hook ./cleanup --preferred-challenges dns -d "${CERT_CN}" \
+  --logs-dir ./logs --work-dir  ./work --config-dir ./config --config cli.ini
+
+echo "Get cert by domain in folder ~/config/live/"
